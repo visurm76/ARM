ebf5d879ba5d4982bc747a1aa08b2211cc3b8cb6
visurm76 <visurm76@gmail.com>
2023-03-21 15:53:22 +0300

    error vydel

--- forest_cult.py
+++ forest_cult.py
@@ -1,11 +1,13 @@
 import sqlite3
 from sys import argv
+import datetime
 
-
+now_year = datetime.datetime.now()
 path_files = 'gubaha.sqlite'
-#select = "SELECT kv,sknr,zk FROM gubaha_kizel"
-class Data(object):
 
+
+# select = "SELECT kv,sknr,zk FROM gubaha_kizel"
+class Data(object):
     def connect_sqlite(self, select):
         """
         Функция считывания данных из базы данных
@@ -14,14 +16,14 @@ class Data(object):
         conn = sqlite3.connect(path_files)
         cur = conn.cursor()
         # Делаем запрос к базе данных и выбираем нужные столбцы из таблицы"SELECT kv,sknr,zk FROM gubaha_vydel"
-        cur.execute(select) # "SELECT kv,sknr,zk FROM gubaha_kizel"
+        cur.execute(select)  # "SELECT kv,sknr,zk FROM gubaha_kizel"
         # Результат запроса в виде списка кортежей
         results = cur.fetchall()
         conn.close()
         return results
 
-class Culture(object):
 
+class Culture(object):
     def __init__(self):
         self.other = Data()
 
@@ -30,26 +32,38 @@ class Culture(object):
         Функция выбирает лесные культуры и сравнивает год посадки и возраст
         :return:
         """
-        kz_culture = ((31,10),(8,9))
+        kz_culture = ((31, 10), (8, 9))
         array = []
-        dict_culture = {}
+
+        """ Выборка из БД выделов с кат. земель - лесные культуры, насаждения с примесью л/к """
+
         for i in self.other.connect_sqlite("SELECT kv,sknr,zk,amz1,amz2,amz3,amz4,\
                                                    kil1,kil2,kil3,kil4,dm11 FROM gubaha_kizel"):
+            if i[2] == 31 or i[2] == 10 and i[3] < 25:
+                age = 2000
+            else:
+                age = 1900
+
             if i[2] in kz_culture[0]:
-                if i[2] == 31:
-                    age = 2000
-                else:
-                    age = 1900
                 array.append([int(i[0]), i[1], i[2], i[3], int(i[11]) + age])
             else:
                 if i[2] in kz_culture[1]:
                     if i[8] == 3:
-                        array.append([int(i[0]), i[1], i[2], i[4], int(i[11]) + 1900])
+                        array.append([int(i[0]), i[1], i[2], i[4], int(i[11]) + age])
                     elif i[9] == 3:
-                        array.append([int(i[0]), i[1], i[2], i[5], int(i[11]) + 1900])
+                        array.append([int(i[0]), i[1], i[2], i[5], int(i[11]) + age])
                     elif i[10] == 3:
-                        array.append([int(i[0]), i[1], i[2], i[6], int(i[11]) + 1900])
-        return array
+                        array.append([int(i[0]), i[1], i[2], i[6], int(i[11]) + age])
+
+            """ Проверяем вычисленный возраст по году посадки"""
+            error_num = []
+            for num in array:
+                age_culture = now_year.year - num[4]
+                if age_culture == num[3]:
+                    continue
+                else:
+                    error_num.append([num[0], num[1]])
+        return error_num
 
 
 class Breeds(object):
@@ -67,10 +81,12 @@ class Breeds(object):
 class Age(Culture):
     pass
 
+
 class Tax(Culture):
     pass
 
+
 s = Data()
 d = Culture()
-#print(s.connect_sqlite())
+# print(s.connect_sqlite())
 print(d.filter_bd())
